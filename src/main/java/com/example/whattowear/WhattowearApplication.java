025d9b2b4ed33efcce875d39121aee59aaa5010f
JLkn <107927104+JLkn@users.noreply.github.com>
2024-01-13 13:59:44 +0100

    First Commit

---
+++ src/main/java/com/example/whattowear/WhattowearApplication.java
@@ -0,0 +1,13 @@
+package com.example.whattowear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattowearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhattowearApplication.class, args);
+	}
+
+}
